25d97f3ed30fe99467feaeeb747f51a639f10904
Kay-Uwe (Kiwi) Lorenz <kiwi@franka.dyndns.org>
2017-09-18 06:23:03 +0200

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@
 
 from distutils.core import setup
 
-setup(name='Confluence API and CLI',
-    version='0.0.1',
+setup(
+    name='confluence-tool',
+    version='0.0.2',
     description='Confluence API and CLI',
     author='Kay-Uwe (Kiwi) Lorenz',
     author_email='kiwi@franka.dyndns.org',
+    install_requires=[
+            'requests',
+            'keyring',
+            'keyrings.alt',
+            'html5print',
+            'pyquery',
+        ],
     #url='https://www.python.org/sigs/distutils-sig/',
     packages=['confluence_tool', ],
     license="MIT",
