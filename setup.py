bdaadb50d0c40cd56df4a50b6162af51f02e359c
Kay-Uwe (Kiwi) Lorenz <kiwi@franka.dyndns.org>
2017-11-30 11:39:37 +0100

    set version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='confluence-tool',
-    version='0.0.13',
+    version='0.0.14',
     description='Confluence API and CLI',
     author='Kay-Uwe (Kiwi) Lorenz',
     author_email='kiwi@franka.dyndns.org',
