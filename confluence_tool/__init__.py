f99d6a4582a68e4f94544fe6ad447cbae4503918
Kay-Uwe (Kiwi) Lorenz <kiwi@franka.dyndns.org>
2019-06-06 15:31:35 +0200

    fix dependency issue

--- confluence_tool/__init__.py
+++ confluence_tool/__init__.py
@@ -2,8 +2,12 @@
 This module provides the main API of Confluence.  You can either import confluence_cli for having a confluence CLI or you import the API.
 """
 
-from .confluence_api import ConfluenceError, ConfluenceAPI
-from .cli import command, arg, main
+try:
+    from .confluence_api import ConfluenceError, ConfluenceAPI
+    from .cli import command, arg, main
+except ImportError:
+    # this may happen in setup due to dependency issues
+    pass
 
 def argparse():
     return command.argparser
